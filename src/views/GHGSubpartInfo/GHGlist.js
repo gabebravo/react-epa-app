7e60ce8d2a219f0d88a4ad3b76d61253a771b222
Gabe Bravo <gabecbravo@gmail.com>
2020-03-16 16:08:08 -0400

    Got a basic version of the infinite scrool working

--- src/views/GHGSubpartInfo/GHGlist.js
+++ src/views/GHGSubpartInfo/GHGlist.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper, Typography } from '@material-ui/core';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,14 +12,66 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function GHGlist({ apiData }) {
-  console.log('apiData', apiData);
+export default function GHGlist() {
   const classes = useStyles();
+  const [loadMore, setLoadMore] = useState(true);
+  const [rowObj, setRowObj] = useState({ rowStart: 0, rowEnd: 9 });
+
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    const { rowStart, rowEnd } = rowObj;
+    const GHG_URL = `https://enviro.epa.gov/enviro/efservice/HH_SUBPART_LEVEL_INFORMATION/REPORTING_YEAR/=/2018/rows/${rowStart}:${rowEnd}/JSON`;
+    async function fetchPost() {
+      try {
+        // attempt fetch
+        const apiData = await axios.get(GHG_URL).then(res => res.data);
+        setData(prevState => [...prevState, ...apiData]); // set data
+        setRowObj(prevState => ({
+          rowStart: prevState.rowEnd + 1,
+          rowEnd: prevState.rowEnd + 10
+        }));
+      } catch (error) {
+        setError(error); // set error
+      } finally {
+        // runs last only under try/pass condiiton >> resets loading
+        setIsLoading(false);
+      }
+    }
+
+    // set the loading flag & make api call
+    setIsLoading(true);
+    fetchPost();
+  };
+
+  useEffect(() => {
+    const list = document.getElementById('list');
+    window.addEventListener('scroll', () => {
+      if (
+        window.scrollY + window.innerHeight >
+        list.clientHeight + list.offsetTop
+      ) {
+        setLoadMore(true);
+      }
+    });
+    return () => window.removeEventListener('scroll');
+  }, []);
+
+  useEffect(() => {
+    if (loadMore) {
+      getData();
+      setLoadMore(false);
+    }
+  }, [loadMore]);
+
+  console.log('data', data);
 
   return (
-    <div className={classes.root}>
-      {apiData &&
-        apiData.map(
+    <div id="list" className={classes.root}>
+      {data.length > 0 &&
+        data.map(
           ({
             FACILITY_ID,
             REPORTING_YEAR,
--- src/views/GHGSubpartInfo/index.js
+++ src/views/GHGSubpartInfo/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import useFetch from '../../hooks/useFetch';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper } from '@material-ui/core';
 import Skeleton from '../../components/SKLoader';
 import Title from '../../components/Title';
 import GHGlist from './GHGlist';
-
-const GHG_URL =
-  'https://enviro.epa.gov/enviro/efservice/HH_SUBPART_LEVEL_INFORMATION/REPORTING_YEAR/=/2018/rows/0:9/JSON';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,20 +21,20 @@ const Error = () => <Paper>Woops... something went wrong!</Paper>;
 
 export default function GHGSubpartInfo() {
   const classes = useStyles();
-  const { loading, error, data } = useFetch(GHG_URL);
   return (
     <>
       <Title text="GHG SUBPART INFO" />
       <div className={classes.root}>
         <Grid container justify="center" spacing={3}>
           <Grid item xs={12} sm={8}>
-            {loading ? (
+            <GHGlist />
+            {/* {loading ? (
               <Skeleton />
             ) : error ? (
               <Error />
             ) : data ? (
-              <GHGlist apiData={data} />
-            ) : null}
+              <GHGlist />
+            ) : null} */}
           </Grid>
         </Grid>
       </div>
